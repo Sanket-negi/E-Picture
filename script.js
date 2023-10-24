9b244778dba0e2edb80642fc14e549c284de9ead
Sanket Singh Negi <124484523+Sanket-negi@users.noreply.github.com>
2023-10-25 00:04:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Add this JavaScript code to create the slideshow functionality
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Change slides every 3 seconds
+}
+
+showSlides();
